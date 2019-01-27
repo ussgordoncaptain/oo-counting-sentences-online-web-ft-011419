6bc99fb1ad46c7fe9b23451eddd758bd7a664c0a
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-27 16:44:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,6 +19,13 @@ class String
     neo = self.join(".")
     temp_arr2 = neo.split("?")
     last_sent = neo.join(".")
-    sentences_arr = last_sent.split("")
+    sentences_arr = last_sent.split(".")
+    empty_str_count = 0 
+    sentences_arr.each do |sent|
+      if sent.length == 0 
+        count += 1
+      end
+    end
+    return sentences_arr.size - count
   end
 end
