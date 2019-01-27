0f13e3821f4b38ea246bfce3d5f15acfc94c66ea
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-27 16:48:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    temp_arr = self.split("!")
+    neo = temp_arr.join(".")
+    temp_arr2 = neo.split("?")
+    last_sent = temp_arr2.join(".")
+    sentences_arr = last_sent.split(".")
+    empty_str_count = 0 
+    sentences_arr.each do |sent|
+      if sent.length == 0 
+        empty_str_count += 1
+      end
+    end
+    foo =  sentences_arr.length 
+    foo - empty_str_count
   end
 end
